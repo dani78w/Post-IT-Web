ffbfd37e0c0441a44c50fab88e24a0e087ef41a5
dani78w <danielarribas78w@gmail.com>
2022-10-26 13:45:53 +0200

    InterfazWeeee

--- parallax.js
+++ parallax.js
@@ -1,19 +1,19 @@
 
 var container = document.getElementById('parallaxContainer');
 
-
-
 var image = document.getElementById('parallax');
         image.style.position.right='0';
-        image.style.width='300px';
+        image.style.width='40vw';
+        image.style.height='145px';
         image.style.display="grid";
         image.style.grid='column: 1';
         image.style.position='relative';
         
+        
         new simpleParallax(image, {
             overflow:false,
-            scale:3,
-            delay: 2,
+            scale:4,
+            delay:4,
             transition: 'cubic-bezier(0,0,0,1)'
         });
 
