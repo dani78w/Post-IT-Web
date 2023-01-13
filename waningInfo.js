8c6351ad9e804fb5206c05129ec452496cd63150
Daniel Arribas <danielarribas78w@gmail.com>
2023-01-13 14:10:21 +0100

    Panel

--- waningInfo.js
+++ waningInfo.js
@@ -1,6 +1,7 @@
 var waningButton = document.getElementById('botonWarning');
 var waningPanel = document.getElementById('infoWarning');
 var body = document.getElementById('body');
+var cerrar = document.getElementById('warningCerrar');
 
 
 waningButton.style.cursor = 'pointer';
@@ -10,4 +11,12 @@ waningButton.onclick = function() {
     waningPanel.style.animationPlayState="running"
     body.style.backgroundColor=" #02ffc8"
     
+}
+cerrar.style.cursor = 'pointer';
+cerrar.onclick = function() {
+
+    waningPanel.style.marginTop = "-1000vh"
+    waningPanel.style.animationPlayState="paused"
+    body.style.backgroundColor="yellow"
+
 }
